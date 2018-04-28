48f2d1d650a9f3e4b0628b00f4445ed74deb5b1c
cailei <cailei@aprbrother.com>
2018-04-28 19:28:07 +0800

    init

--- tests/GuzzleTest.php
+++ tests/GuzzleTest.php
@@ -9,11 +9,57 @@
 require  __DIR__."/../vendor/autoload.php";
 
 
-$response = \Dancesmile\Dttp::withHeaders([
-    "test" =>"htader"
-])->post("http://localhost:9090/post?hello=world",[
-    "name" => "cailei"
-])->body();
 
 
-var_dump($response);
+
+
+$response = \Dancesmile\Dttp::get("http://dev.youyouchina.cn/api/v1/index/adsense")->json();
+
+$response = \Dancesmile\Dttp::post("http://dev.youyouchina.cn/api/v1/index/adsense")->status();
+
+
+
+$data = [
+    "phone"=>15370536039,
+    "code"=>1160,
+    "password"=>md5("137681502"),
+    "deviceId"=>"asd",
+    "type" => 0
+];
+$response = \Dancesmile\Dttp::asjson()->post("http://dev.youyouchina.cn/api/v1/user/login",$data)->json();
+
+
+$token = $response["result"]['token'];
+
+$fiel = file_get_contents("http://www.youyouchina.cn/web/images/code.png");
+
+dd($fiel);
+
+
+$formData = [
+    [
+        "name" => "file",
+        "contents" => fopen("http://www.youyouchina.cn/web/images/code.png","r"),
+        "headers"=> [
+            "Content-Type" => "image/jpeg"
+        ]
+    ],
+    [
+        "name" => "json",
+        "contents" => json_encode([
+            "token" => $token,
+            "nickname" => "testMutipart"
+        ])
+    ]
+];
+$response = \Dancesmile\Dttp::asMultipart()->post('http://dev.youyouchina.cn/api/v1/user/edit_user',$formData)->json();
+
+
+dd($response);
+
+
+
+
+
+
+
